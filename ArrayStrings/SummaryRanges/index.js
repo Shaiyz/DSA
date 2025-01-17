0077c9d8b44c351dc7479cc589b010a9e68dd635
Shaiyz Khan <shaiyzkhan@MacBook-Pro-Shaiyz.local>
2025-01-17 11:47:15 +0500

    UPDATE: initial commit

---
+++ ArrayStrings/SummaryRanges/index.js
@@ -0,0 +1,18 @@
+var summaryRanges = function(nums) {
+    const result = [];
+    if(nums.length===0) return []
+    let i=0
+    while(i<nums.length){
+        start=nums[i]
+        while(i+1<nums.length && nums[i]+1==nums[i+1]){
+            i++
+        }
+        if(start===nums[i]){
+            result.push(`${start}`)
+        }else{
+            result.push(`${start}->${nums[i]}`)
+        }
+        i++
+    }
+    return result
+};
